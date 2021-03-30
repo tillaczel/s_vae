1b1127bb9cc4c5b3a6bc42ac661bfbc8cad85574
stas97 <stas97@hotmail.com>
2021-03-30 17:22:28 +0200

    Sample hypersphere for vMF

--- s_vae/models/backbone/utils.py
+++ s_vae/models/backbone/utils.py
@@ -1,10 +1,30 @@
-from torch import nn
+import torch
 
-
-class Reshape(nn.Module):
+class Reshape(torch.nn.Module):
     def __init__(self, shape):
         super().__init__()
         self.shape = shape
 
     def forward(self, x):
         return x.view(-1, *self.shape)
+
+
+def sample_hypersphere(n, dim, R = 1):
+    """ 
+    This function will sample from uniform distribution on the *dim*-dimensional unit sphere. 
+    The method used comes from  "Computer Generation of Distributions on
+    the m-sphere" By GARY ULRICH. 
+    n: number of points to sample
+    dim: the dimension of the sphere.
+    R: radius of the sphere
+    """
+
+    distrib = torch.distributions.MultivariateNormal(torch.zeros(dim), torch.eye(dim))
+    dim_arr = torch.empty(dim, n)
+
+    for i in range(n):
+        values = distrib.sample()
+        assert(R>0)
+        dim_arr[:,i] = (values*R)/torch.linalg.norm(values, ord= 2)
+    
+    return  dim_arr
