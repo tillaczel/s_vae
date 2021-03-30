d319fdd6d1906785a719b3abb64526731cc99210
stas97 <stas97@hotmail.com>
2021-03-30 22:10:37 +0200

    Update README.md

---
+++ s_vae/models/backbone/unif_on_sphere.py
@@ -0,0 +1,46 @@
+import torch 
+import math
+from torch.distributions.distribution import Distribution
+
+class UnifOnSphere(Distribution):
+    """
+    Creates a uniform distribution on a m-dimensional hypersphere
+
+    Args:
+    ndim (Number, Tensor): Dimension of the hypersphere   
+    device "cpu"/"gpu": device to have the torch.tensor stored on 
+    """
+
+    has_rsample = False
+    support = torch.distributions.constraints.real
+    _mean_carrier_measure = 0
+
+    def __init__(self, ndim, device = "cpu"):
+        super(UnifOnSphere, self).__init__(
+            torch.Size([ndim]))
+        self._ndim = ndim
+        self._device = device
+    
+    @property
+    def dim(self):
+        return self.dim
+
+    @property
+    def device(self):
+        return self._device
+
+    @device.setter
+    def device(self, val):
+        self._device = val if isinstance(val, torch.device) else torch.device(val)
+
+    def sample(self, sample_shape=torch.Size(), R = 1):
+
+        shape = sample_shape if isinstance(sample_shape, torch.Size) else torch.Size([sample_shape])
+        MultNorm = torch.distributions.MultivariateNormal(torch.zeros(self._ndim), torch.eye(self._ndim)).sample(shape).to(self._device)
+        
+        assert(R>0)
+        return (MultNorm*R)/torch.linalg.norm(MultNorm, dim = -1,  ord= 2, keepdim = True)
+
+    def entropy(self):
+        lgamma = torch.lgamma(torch.tensor([(self._ndim + 1) / 2]).to(self.device))
+        return math.log(2) + ((self._ndim + 1) / 2) * math.log(math.pi) - lgamma
