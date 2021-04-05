93bdfb0b1a03a0406873e5172ff7c5a2dc7178c8
stas97 <stas97@hotmail.com>
2021-04-05 13:57:46 +0200

    first S-VAE draft

--- s_vae/models/s_vae/s_vae.py
+++ s_vae/models/s_vae/s_vae.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from unif_on_sphere import  UnifOnSphere
+from vMF import vMF
 
 class SVAE(nn.Module):
     def __init__(self, encoder: nn.Module, decoder: nn.Module, encoder_out_dim: int, latent_dim: int, kl_coeff: float):
@@ -12,49 +14,48 @@ class SVAE(nn.Module):
         self.kl_coeff = kl_coeff
 
         self.fc_mu = nn.Linear(encoder_out_dim, latent_dim)
-        self.fc_var = nn.Linear(encoder_out_dim, latent_dim)
+        self.fc_kappa = nn.Linear(encoder_out_dim, 1) # concentration parameter is just a scalar
 
     def encode(self, x):
         encoded = self.encoder(x)
 
         mu = self.fc_mu(encoded)
-        log_var = self.fc_var(encoded)
+        mu = mu/torch.linalg.norm(mu, ord = 2, keepdim = True, dim= -1) # make mu a vector on the sphere
 
-        return mu, log_var
+        kappa = self.fc_kappa(encoded)
+
+        return mu, kappa
 
     def decode(self, z):
         reconstructed = self.decoder(z)
         return reconstructed
 
     def _forward(self, x):
-        mu, log_var = self.encode(x)
-        p, q, z = self.sample(mu, log_var)
+        mu, kappa = self.encode(x)
+        p, q, z = self.sample(mu, kappa)
         x_hat = self.decode(z)
-        return x_hat, mu, log_var, p, q, z
+        return x_hat, mu, kappa, p, q, z
 
     def forward(self, x):
         x_hat, mu, log_var, p, q, z = self._forward(x)
         return x_hat, z
 
     def step(self, x):
-        x_hat, mu, log_var, p, q, z = self._forward(x)
+        x_hat, mu, kappa, p, q, z = self._forward(x)
 
         loss_recon = F.mse_loss(x_hat, x, reduction='mean')
 
-        log_qz = q.log_prob(z)
-        log_pz = p.log_prob(z)
-
-        loss_kl = log_qz - log_pz
-        loss_kl = loss_kl.mean()
-        loss_kl *= self.kl_coeff
+        loss_kl = torch.distributions.kl.kl_divergence(q,p).mean()
 
         loss = loss_kl + loss_recon
 
         return {'loss': loss, 'loss_recon': loss_recon, 'loss_kl': loss_kl}
 
-    def sample(self, mu, log_var):
-        std = torch.exp(log_var / 2)
-        p = torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(std))
-        q = torch.distributions.Normal(mu, std)
-        z = p.rsample() * std + mu
+
+    def sample(self, mu, kappa):
+
+        p = UnifOnSphere(self.latent_dim)
+        q = vMF(mu, kappa)
+        z = q.rsample()
+
         return p, q, z
