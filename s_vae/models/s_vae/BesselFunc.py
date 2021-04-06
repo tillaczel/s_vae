Fixed negative KL and some Bessel Issues

s_vae/models/s_vae/BesselFunc.py
@@ -12,13 +12,13 @@ class BesselFunc(torch.autograd.Function):
         # Save for the backward pass
         ctx._nu = nu
         ctx.save_for_backward(kappa)
-        
+
         if math.isclose(nu, 0.0):
-            return torch.from_numpy(scipy.special.i0(kappa.detach().numpy()))
+            return torch.from_numpy(scipy.special.i0e(kappa.detach().numpy()))
         elif math.isclose(nu, 1.0):
-            return torch.from_numpy(scipy.special.i1(kappa.detach().numpy()))
+            return torch.from_numpy(scipy.special.i1e(kappa.detach().numpy()))
         else:
-            return torch.from_numpy(scipy.special.iv(nu.detach().numpy(), kappa.detach().numpy()))
+            return torch.from_numpy(scipy.special.ive(nu.detach().numpy(), kappa.detach().numpy()))
     
     @staticmethod
     def backward(ctx, grad_out):

s_vae/models/s_vae/vMF.py
@@ -4,8 +4,6 @@ from torch.distributions.distribution import Distribution
 from torch.distributions.beta import Beta
 from torch.distributions.uniform import Uniform
 from torch.distributions.kl import register_kl
-#from BesselFunc import Bessel
-#from unif_on_sphere import UnifOnSphere
 from s_vae.models.s_vae.BesselFunc import Bessel
 from s_vae.models.s_vae.unif_on_sphere import UnifOnSphere
 
@@ -132,7 +130,7 @@ class vMF(Distribution):
 
     def entropy(self):
         output = (
-            -self.scale
+            self.scale
             * Bessel(self.ndim/ 2, self.scale)
             / Bessel((self.ndim/ 2) - 1, self.scale)
         )
@@ -140,7 +138,7 @@ class vMF(Distribution):
         return output+ self._log_normalization()
 
     def _log_normalization(self):
-        output = -(
+        output = (
             (self.ndim/ 2 - 1) * torch.log(self.scale)
             - (self.ndim/ 2) * math.log(2 * math.pi)
             - torch.log(Bessel(self.ndim/ 2 - 1, self.scale))
@@ -150,7 +148,7 @@ class vMF(Distribution):
 
 @register_kl(vMF, UnifOnSphere)
 def _kl_vmf_uniform(vmf, unisphere):
-    return -vmf.entropy() + unisphere.entropy()
+    return vmf.entropy() + unisphere.entropy()
 
 
 if __name__ == "__main__":