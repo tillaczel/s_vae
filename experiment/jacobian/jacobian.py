Fixed more mistakes in Jacobian and first working draft of synth data

experiment/jacobian/jacobian.py
@@ -60,17 +60,16 @@ class Jacobian_experiment():
     def run_experiment(self):
         
         z = self.sample_latent_points()
+        print(z.shape)
         jacobians = self.calc_jacobian(z)
         determinants = self.calc_determinants(jacobians)
 
         return determinants
 
-
-
     def sample_latent_points(self):
         if self.sampling == 'prior':
             if self.name == 's_vae':
-                distribution = UnifOnSphere(model.latent_dim, model._device)
+                distribution = UnifOnSphere(self.model.latent_dim, self.device)
             else:
                 distribution = torch.distributions.Normal(torch.zeros(self.model.latent_dim), torch.ones(self.model.latent_dim))
 
@@ -78,15 +77,19 @@ class Jacobian_experiment():
             
         else:
             x = self.get_data()
-            # kappa_or_log_var because it depends on the incoder
+
             mu_param_vector = []
             kappa_or_log_var_param_vector = []
             for batch,label in x:
                 mu, kappa_or_log_var = self.model.encode(batch)
                 mu_param_vector.append(mu)
-                kappa_or_log_var_param_vector.append(mu)
+                kappa_or_log_var_param_vector.append(kappa_or_log_var)
             mu = torch.stack(mu_param_vector)
             kappa_or_log_var = torch.stack(kappa_or_log_var_param_vector)
+
+            if self.name == 's_vae':
+                mu = mu.squeeze()
+                kappa_or_log_var = kappa_or_log_var.squeeze(dim = 2)
 
             p, q, z = self.model.sample(mu, kappa_or_log_var)
             
@@ -94,8 +97,15 @@ class Jacobian_experiment():
 
     def calc_jacobian(self,samples):
         jacobians = []
+
+        printcounter = 0
         for sample in range(len(samples)):
+        #for sample in range(2): Debug line
             jacobians.append(jacobian(self.model.decode, samples[sample]).squeeze().view(784,-1))
+            if printcounter == 1000:
+                print("Another 1000 Jacobians")
+                printcounter = 0
+            printcounter += 1
         return jacobians
 
     def calc_determinants(self,jacobians):
@@ -111,7 +121,6 @@ class Jacobian_experiment():
     def get_data(self):
         if self.data == 'MNIST':
             train_set, val_set, test_set = create_MNIST(self.config) # with some paramters. These are dataloaders.
-
             if self.data_split == 'train':
                 loader = DataLoader(train_set, batch_size=1, shuffle=False, num_workers=0)
             elif self.data_split == 'val':
@@ -129,8 +138,8 @@ def main(config_path: str):
         config = yaml.load(fd, yaml.FullLoader)
     experiment = Jacobian_experiment(config)
     output = experiment.run_experiment()
-    
-    with open('../../local/jacobian/experiment_outcome.pickle', 'wb') as handle:
+    path = '../../local/jacobian/' + config['experiment']['name']+'_'+config['jacobian']['sampling']
+    with open(path, 'wb') as handle:
         pickle.dump(output, handle)
 
 

s_vae/data/synthetic_hypersphere.py
@@ -1,12 +1,18 @@
+import sys
+sys.path.append('../../')
+
 import numpy as np
 import random
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, random_split
 import os
+import math 
 
+from s_vae.models.s_vae.unif_on_sphere import UnifOnSphere
+from s_vae.models.s_vae.vMF import vMF
 
-def generate_latent_data(n, dim=3, R=1, datadist=None, seed=0):
+def generate_latent_data(n, dim=2, R=1, datadist=None, vMF_centroids = 5 , seed=0, device = 'cpu'):
     """
     A function for generating n randomly distributed
     points on the surface of a hypersphere.
@@ -14,12 +20,11 @@ def generate_latent_data(n, dim=3, R=1, datadist=None, seed=0):
     n : number of points generated
     dim : dimension of the hypersphere
     R : radius of the hypersphere
-    datadist : a list of strings that may include 'uniform'
-               and 'skew'. 'uniform' is a random uniform
-               distribution. 'skew' is another n/2 data
-               points distributed in one m'th quadrant
-               (where all vectors have positive values)
-               of the hypersphere.
+    datadist : a list of strings that may include 'uniform' and 'vMF'. 
+            'uniform' is a uniform distribution on a hypersphere. 
+            'vMF' is the von Mises-Fischer distribution on a hypersphere.
+    vMF_centroids: number of centroids for the vMF distributions. Essentially means we sample from vMF_centroids vMF distributions with 
+                   vMF_centroids number of mean-vectors.
     """
     if datadist is None:
         datadist = ['uniform', 'skew']
@@ -27,28 +32,32 @@ def generate_latent_data(n, dim=3, R=1, datadist=None, seed=0):
     np.random.seed(seed)
 
     dim_arr = [[] for _ in range(dim)]
-
+            
     if 'uniform' in datadist:
-        for _ in range(n):
-            r = np.random.random(dim)**2
-            r = r/(r.sum())
-            coords = np.sqrt(r)
-            for i in range(dim):
-                if bool(random.getrandbits(1)):
-                    x = -1 * coords[i]
-                else:
-                    x = coords[i]
-                dim_arr[i].append(x*R)
+        distribution = UnifOnSphere(dim, device)
+        samples = distribution.sample(torch.Size((n,)))
 
-    if 'skew' in datadist:
-        for _ in range(int(n/2)):
-            rp = np.random.random(dim)
-            rp = rp/(rp.sum())
-            coords = np.sqrt(rp)
-            for i in range(dim):
-                dim_arr[i].append(coords[i]*R)
+    if 'vMF' in datadist:
+        # Only works for a circle right now
+        assert(dim == 2)
+
+        mean_vector_angles = np.arange(0, 360, 360/vMF_centroids)
+        centroids = []
+        for angle in mean_vector_angles:
+            x1 =  math.cos(math.radians(angle+5))
+            x2 =  math.sin(math.radians(angle+5))
+            centroids.append(torch.tensor([x1,x2]))
+
+        mu = torch.stack(centroids)
+        kappa = torch.ones(torch.Size((vMF_centroids,1)))
+        distribution = vMF(mu, kappa)
     
-    return np.array(dim_arr).T
+    samples = []
+    for sample in range(int(n/vMF_centroids)):
+        samples.append(distribution.rsample(sample_shape=mu.shape))
+    
+    samples = torch.stack(samples).view(-1,2)
+    return samples
 
 
 class Nonlinearity(nn.Module):
@@ -115,9 +124,3 @@ class SyntheticHypersphereDataset(Dataset):
     def __getitem__(self, idx):
         x_i = self.x[idx]
         return x_i, torch.Tensor(np.zeros(x_i.shape))
-
-
-
-
-
-